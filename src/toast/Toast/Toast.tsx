1fffe7e15f45131269442246e9addcca99a516ee
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-02-11 22:11:50 +0100

    refactor: rename Toast component to ToastComponent and memoize for performance

--- src/toast/Toast/Toast.tsx
+++ src/toast/Toast/Toast.tsx
@@ -1,16 +1,17 @@
 import { useToastService } from '@Front/toast/useToast/useToastService';
 import { useToastSelector } from '@Front/toast/useToastSelector/useToastSelector';
 import { Alert } from '@pplancq/shelter-ui-react';
+import { memo } from 'react';
 
+import '@pplancq/shelter-ui-css/css/components/alert.css';
 import '@pplancq/shelter-ui-css/css/components/button.css';
 import '@pplancq/shelter-ui-css/css/components/icon.css';
-import '@pplancq/shelter-ui-css/css/components/alert.css';
 
 export type ToastProps = {
   id: number;
 };
 
-export const Toast = ({ id }: ToastProps) => {
+const ToastComponent = ({ id }: ToastProps) => {
   const toast = useToastSelector(t => t.getToastById(id));
   const toastService = useToastService();
 
@@ -20,3 +21,5 @@ export const Toast = ({ id }: ToastProps) => {
 
   return <Alert title={toast.message} onClose={() => toastService.removeToast(id)} />;
 };
+
+export const Toast = memo(ToastComponent);
