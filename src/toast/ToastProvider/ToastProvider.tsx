26e07862549ff1f06a39b924d24ce4e5296d3a14
GuillaumeKESTEMAN <kesteman.guillaume@gmail.com>
2026-03-01 17:42:27 +0100

    refactor: remove unused global toast service assignment in ToastProvider

--- src/toast/ToastProvider/ToastProvider.tsx
+++ src/toast/ToastProvider/ToastProvider.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle,@typescript-eslint/ban-ts-comment */
 import { ToastContext } from '@Front/toast/ToastProvider/ToastContext';
 import { ToastService } from '@Front/toast/ToastService/ToastService';
 import { type PropsWithChildren, useRef } from 'react';
@@ -10,9 +9,6 @@ export type ToastProviderProps = PropsWithChildren & {
 
 export const ToastProvider = ({ children, defaultDuration }: ToastProviderProps) => {
   const toastRef = useRef({ toast: new ToastService(defaultDuration) });
-
-  // @ts-expect-error
-  global.__toastService = toastRef.current.toast;
 
   return (
     <ToastContext value={toastRef.current}>
